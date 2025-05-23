d994f859a939e82d177b770e0da60a4210d1798f
Dorathe3xpl0r3r <qwertyuiopvalerio@gmail.com>
2025-05-23 15:05:40 +0200

    Update test.py

--- Python/test.py
+++ Python/test.py
@@ -3,3 +3,10 @@ def faculteit(n):
     while n > 1:
         totaal *= n
         return n
+if _name_ == "_main_":
+    print(faculteit(0))
+    print(faculteit(1))
+    print(faculteit(2))
+    print(faculteit(3))
+    print(faculteit(4))
+    print(faculteit(5))
